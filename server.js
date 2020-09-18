9a7d917474d47defc3061f294b524176bb4e5474
attiliogiglio <attilio.g93@gmail.com>
2020-09-18 20:30:14 -0300

    testing

--- server.js
+++ server.js
@@ -2,9 +2,27 @@ const express = require('express');
 
 const app = express();
 
+const dataBase = {
+    users: [
+        {
+            is:'123',
+            name:'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries:0,
+            joined: new Date()
+        }
+    ]
+}
+
 app.get('/', (req, res) => {
     res.send('this is working')
 })
+
+app.post('/signin', (req, res) => {
+    res.send('signing')
+})
+
 
 app.listen(4000, ()=>{
     console.log('app is runnning on port 4000')
