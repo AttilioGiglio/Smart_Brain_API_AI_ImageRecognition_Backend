6422ae27b8b253ee4f8646afd70f23ea7615d311
attiliogiglio <attilio.g93@gmail.com>
2020-09-21 18:07:46 -0300

    more

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
-
 const register = require('./Controllers/register');
 const signin = require('./Controllers/signin');
 const profile = require('./Controllers/profile');
