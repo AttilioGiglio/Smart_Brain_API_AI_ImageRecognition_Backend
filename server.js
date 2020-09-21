ca358278b90d69753224d6c61a46008dbe2399b7
attiliogiglio <attilio.g93@gmail.com>
2020-09-20 21:38:47 -0300

    getting user profile to DB

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const knex = require('knex');
 
 const db = knex({
     client: 'pg',
-        connection: {
-          host : '127.0.0.1', //localhost
-          user : 'postgres',
-          password : 'postgres',
-          database : 'smart-brain'
-        }
-  });
+    connection: {
+        host: '127.0.0.1', //localhost
+        user: 'postgres',
+        password: 'postgres',
+        database: 'smart-brain'
+    }
+});
 
 // db.select('*').from('users').then(data => {
 //     console.log(data)  /* --> this log giveme the password error :( */
@@ -57,7 +57,7 @@ app.get('/', (req, res) => {
 
 app.post('/signin', (req, res) => {
     bcrypt.compare("chucha", '$2a$10$h6yIBQ8Jlq6MIAJFQv6eRe2XmQGRMM4PV.lvoIcSwME0u/oJaAaIC', function (err, res) {
-       console.log('first guess', res)
+        console.log('first guess', res)
     });
     bcrypt.compare("veggies", '$2a$10$h6yIBQ8Jlq6MIAJFQv6eRe2XmQGRMM4PV.lvoIcSwME0u/oJaAaIC', function (err, res) {
         console.log('second guess', res)
@@ -76,29 +76,38 @@ app.post('/register', (req, res) => {
     //     console.log(hash);
     // });
     db('users').returning('*').insert({
-        email:email,
-        name:name,
+        email: email,
+        name: name,
         joined: new Date()
     }).then(user => {
         res.json(user[0])
         // dataBase.users[dataBase.users.length-1], instead of .returning('*').. reserver word from knex
-    }).catch(err =>res.status(400).json('unable to register'))
+    }).catch(err => res.status(400).json('unable to register'))
     // res.json(dataBase.users[dataBase.users.length - 1])
 })
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    dataBase.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
+    // let found = false;
+    db.select('*').from('users').where({ id:id })
+    .then(user => {
+        if(user.length){
+            res.json(user[0])
         }
-    })
-    if (!found) {
+        else{
+            res.status(400).json('not found')
+        }
+    }).catch(err => res.status(400).json('error getting user'))
+    // dataBase.users.forEach(user => {
+    //     if (user.id === id) {
+    //         found = true;
+    //         return res.json(user);
+    //     }
+    // })
+    // if (!found) {
 
-        res.status(404).json('no such user')
-    }
+    //     res.status(404).json('no such user')
+    // }
 })
 
 app.put('/image', (req, res) => {
