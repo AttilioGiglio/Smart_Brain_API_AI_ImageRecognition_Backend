4632a1ae7f2ee0d31217ba7f268b5788b955dc74
attiliogiglio <attilio.g93@gmail.com>
2020-09-21 16:10:34 -0300

    passing apikey from front to back

--- Controllers/image.js
+++ Controllers/image.js
@@ -1,3 +1,17 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '830df3e3c47a4135989c97e73c30b3d9'
+});
+
+const handleApiCall = (req,res)=>{
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data =>{
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     // let found = false;
@@ -20,5 +34,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage:handleImage
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
 };
--- server.js
+++ server.js
@@ -68,6 +68,8 @@ app.get('/profile/:id', (req,res) => {profile.handleProfile(req, res, db)})
 
 app.put('/image', (req,res) => {image.handleImage(req, res, db)})
 
+app.post('/imageurl', (req,res) =>{image.handleApiCall(req,res)})
+
 app.listen(4000)
 
 /*
