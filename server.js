9cda532cf1a052863a527c016cc9081a542e2f0c
attiliogiglio <attilio.g93@gmail.com>
2020-09-21 12:22:12 -0300

    adding hash and email with register to DB login and user

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.post('/register', (req, res) => {
             return trx('users')
             .returning('*')
             .insert({
-                email: loginEmail,
+                email: loginEmail[0],
                 name: name,
                 joined: new Date()
             })
