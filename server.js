b8274ed7e9508418531069f7e0439713cf8406ee
attiliogiglio <attilio.g93@gmail.com>
2020-09-21 18:18:58 -0300

    addin part

--- server.js
+++ server.js
@@ -69,7 +69,9 @@ app.put('/image', (req,res) => {image.handleImage(req, res, db)})
 
 app.post('/imageurl', (req,res) =>{image.handleApiCall(req,res)})
 
-app.listen(4000)
+app.listen(process.env.PORT || 4000, () =>{
+    console.log(`app is running on port ${process.env.PORT}`)
+})
 
 /*
 --> res = this is working
