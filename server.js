d3997b25499ede5eaedf260814edef266a545e79
attiliogiglio <attilio.g93@gmail.com>
2020-09-22 12:34:36 -0300

    change names files

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 const register = require('./controllers/register');
-const signin = require('./controllers/c2');
+const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
@@ -69,7 +69,9 @@ app.put('/image', (req,res) => {image.handleImage(req, res, db)})
 
 app.post('/imageurl', (req,res) =>{image.handleApiCall(req,res)})
 
-app.listen(4000)
+app.listen(process.env.PORT || 4000, () => {
+    console.log(`app is running on port ${process.env.PORT}`)
+})
 
 /*
 --> res = this is working
