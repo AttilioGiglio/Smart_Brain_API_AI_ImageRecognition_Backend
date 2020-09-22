ef2999d635bf95b041bb020add72e9f5baf1beae
attiliogiglio <attilio.g93@gmail.com>
2020-09-22 13:29:24 -0300

    update

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,11 +1,15 @@
 const handleSignin = (db, bcrypt) => (req,res) => {
+    const {email, password} = req.body;
+    if(!email||!password){
+        return res.status(400).json('incorrect form submission');
+    }
     db.select('email', 'hash').from('login')
-    .where("email", "=", req.body.email)
+    .where("email", "=", email)
     .then(data => {
-       const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+       const isValid = bcrypt.compareSync(password, data[0].hash);
         if(isValid){
             return db.select('*').from('users')
-            .where("email", "=", req.body.email)
+            .where("email", "=", email)
             .then(user =>{
                 res.json(user[0])
             })
@@ -21,7 +25,7 @@ const handleSignin = (db, bcrypt) => (req,res) => {
     // bcrypt.compare("veggies", '$2a$10$h6yIBQ8Jlq6MIAJFQv6eRe2XmQGRMM4PV.lvoIcSwME0u/oJaAaIC', function (err, res) {
     //     console.log('second guess', res)
     // });
-    // if (req.body.email === dataBase.users[0].email && req.body.password === dataBase.users[0].password) {
+    // if (email === dataBase.users[0].email && password === dataBase.users[0].password) {
     //     res.json(dataBase.users[0])
     // } else {
     //     res.status(400).json('error logging in')
